5e57d1382dd7a4c7a00f4cc46274357c89cc13c5
lace04 <luisace04@gmail.com>
2024-02-02 09:27:31 -0500

    Add Svelte configuration and update TypeScript configuration

---
+++ src/components/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import {Alert} from 'flowbite-svelte'
+</script>
+
+<Alert type="success" dismissible>
+  <strong>Well done!</strong> You successfully read this important alert message.
+</Alert>
